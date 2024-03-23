824b8dbc2ab9d799900557e415a893af336570cb
Camila Ferreira Rodrigues <131890075+CamilaFerreiraRodrigues@users.noreply.github.com>
2024-03-23 01:41:14 -0300

    Create exercicio01.sql

---
+++ Exercicios/exercicio01.sql
@@ -0,0 +1,29 @@
+Exercicios de banco de dados:
+a) Criar a tabela EMPREGADOS: 
+  MATR  CHAR(6)
+  NOME VARCHAR(12)
+  SOBRENOME VARCHAR(15)
+  DEPT CHAR(10)
+  FONE  VARCHAR(14)
+  DATA_ADM  DATETIME
+  CARGO CHAR(10)
+  SEXO  CHAR(1)
+  IDADE INT
+  SALARIO NUMERIC(10,2)
+  COMISSAO  NUMERIC(10,2) 
+
+b)Inserir  10  registros
+c)Selecionar o sobrenome, primeiro nome, departamento,data de admissão e salário de todos os empregados que ganham mais que R$ 5000.
+d)Qual a média de salários dos funcionários?
+e)Quantos funcionários pertencem ao departamento de TI?
+f)Quantos funcionários temos com cargo igual a PROGRAMADOR?
+g)Qual o somatório dos salários?
+h)Qual o valor do maior salário?
+i)Qual o valor do menor salário?
+j)Atribuir um aumento de 5% aos funcionários do departamento de TI.
+k)Selecionar os cargos (sem duplicidade).
+l)Selecionar os funcionários com idade entre  30  e  40  anos (inclusive).
+m)Qual a média de idades?
+n)Selecionar todos os funcionários com idade entre  20  e  30  anos (inclusive) do departamento de TI.
+o)Selecionar todos os funcionários cujo nome inicia-se pela letra ‘J’.
+p)Selecionar todos os funcionários dos departamentos de TI, Vendas e Marketing.
